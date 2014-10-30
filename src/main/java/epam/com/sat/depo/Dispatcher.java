11ebd9fe2f9eb204b2790ddfa329e0e94ec3a9e7
satbeka <satbeka@gmail.com>
2014-10-30 22:28:30 +0600

    30.10.2014

---
+++ src/main/java/epam/com/sat/depo/Dispatcher.java
@@ -0,0 +1,166 @@
+package epam.com.sat.depo;
+
+import javax.annotation.processing.SupportedOptions;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.*;
+
+public class Dispatcher {
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Train launchTrain(String nameDepo,String numberTrain, int countPassengerVagon,int countCargoVagon){
+        Depo Depo=new Depo(nameDepo);
+        Train T=new Train(numberTrain);
+
+        T.addLokomotiv("TOYOTA","Sat");
+        for (int i = 0; i <countPassengerVagon ; i++) {
+            PassengerVagon P=new PassengerVagon(i);
+            T.addVagon(P);
+            System.out.println("PassengerVagon add id="+P.getIdVagon()+" count of passengers="+P.getCountPassenger()
+                    +" count of cargo="+P.getCountCargo()
+                    +" comfort="+P.getLevelComfort());
+        }
+
+        for (int k = countPassengerVagon; k <countPassengerVagon+countCargoVagon ; k++) {
+            TransportVagon Tv=new TransportVagon(k,"GAZ");
+            T.addVagon(Tv);
+            int y;
+            System.out.println("TransportVagon add id="+Tv.getIdVagon());
+        }
+
+        /*
+        //for (int i = 0; i < totalVagons; i++) {
+        PassengerVagon P1=new PassengerVagon(12,1,2,55);
+        PassengerVagon P2=new PassengerVagon(5,2,1,65);
+        PassengerVagon P3=new PassengerVagon(2,3,11,5);
+        PassengerVagon P4=new PassengerVagon(50,4,5,95);
+        PassengerVagon P5=new PassengerVagon(120,5,0,100);
+        PassengerVagon P6=new PassengerVagon(0,6,0,15);
+        PassengerVagon P7=new PassengerVagon(10,7,5,5);
+
+        OilVagon O1=new OilVagon(80,9,80,"AI96");
+
+        //}
+
+        T.addVagon(P1).addVagon(P2).addVagon(P3).addVagon(P5).addVagon(P8).addVagon(P7).addVagon(O1);
+        */
+
+        Date sysDate = new Date();
+        String sysDTString = sysDate.toString();
+
+        {System.out.println("Train create on="+sysDTString);}
+        return T;
+    }
+    public int calcTotalPassenger(Train T){
+        int TotalPassenger=0;
+        List<Vagon> listTemp;//=new ArrayList<Vagon>();
+        listTemp=T.getList();
+        for (int i = 0; i < T.countVagon(); i++) {
+            Vagon Vg;
+            Vg=listTemp.get(i);
+            if (Vg instanceof PassengerVagon) {
+                PassengerVagon VgP;
+                VgP=(PassengerVagon)Vg;
+                TotalPassenger+=VgP.getCountPassenger();
+            };
+        }
+        {System.out.println("TotalPassenger ="+TotalPassenger);}
+        return TotalPassenger;
+    }
+    public int calcTotalPassengerCargo(Train T){
+        int TotalPassengerCargo=0;
+        List<Vagon> listTemp;//=new ArrayList<Vagon>();
+        listTemp=T.getList();
+        for (int i = 0; i < T.countVagon(); i++) {
+            Vagon Vg;
+            Vg=listTemp.get(i);
+            if (Vg instanceof PassengerVagon) {
+                TotalPassengerCargo+=Vg.getCountCargo();
+            };
+        }
+        {System.out.println("TotalPassengerCargo ="+TotalPassengerCargo);}
+        return TotalPassengerCargo;
+    }
+    public int calcTotalCargo(Train T){
+        int TotalCargo=0;
+        List<Vagon> listTemp;//=new ArrayList<Vagon>();
+        listTemp=T.getList();
+        for (int i = 0; i < T.countVagon(); i++) {
+            Vagon Vg;
+            Vg=listTemp.get(i);
+            TotalCargo+=Vg.getCountCargo();
+        }
+        {System.out.println("TotalCargo ="+TotalCargo);}
+        return TotalCargo;
+    }
+
+    public void sortByComfortLevel(Train T){
+
+        //----
+        ArrayList<Vagon> listVagonSort=T.getList();
+        Collections.sort(listVagonSort, new Comparator<Vagon>() {
+            @Override
+            public int compare(Vagon o1, Vagon o2) {
+                if ((o1 instanceof TransportVagon)) {
+                    return -1;
+                }
+                ;
+                if ((o2 instanceof TransportVagon)) {
+                    return -1;
+                }
+                ;
+                String a = ((PassengerVagon) o1).getLevelComfort();
+                String b = ((PassengerVagon) o2).getLevelComfort();
+                //System.out.println("a="+a+" "+PassengerVagon.getIdComfort(a));
+                //System.out.println("b="+b+" "+PassengerVagon.getIdComfort(b));
+
+                //PassengerVagon.getIdComfort(a);
+                //PassengerVagon.getIdComfort(b);
+
+                return PassengerVagon.getIdComfort(a) > PassengerVagon.getIdComfort(b) ? 1 : a == b ? 0 : -1;
+            }
+        });
+
+        //---
+        System.out.println(" Sort by comfort level is:");
+        for (Vagon V :listVagonSort) {
+            if ((V instanceof PassengerVagon)) {
+                System.out.println("PassengerVagon id=" + V.getIdVagon() + " comfort level= "
+                        + ((PassengerVagon) V).getLevelComfort());
+            }
+
+        }
+        T.setList(listVagonSort);
+
+    }
+
+    static public List<Integer> searchVagons(int fromA, int toB, Train T)
+    {
+        List<Integer> arrVagons=new ArrayList<Integer>();
+        List<Vagon> listTemp;
+        listTemp=T.getList();
+        int cntP;
+
+        for (Vagon V : listTemp) {
+            if ((V instanceof PassengerVagon)) {
+                cntP=((PassengerVagon) V).getCountPassenger();
+                if (cntP>=fromA&&cntP<=toB) {arrVagons.add(V.getIdVagon());};
+
+                //System.out.println("PassengerVagon id=" + V.getIdVagon() + " comfort level= "
+                //      + ((PassengerVagon) V).getLevelComfort());
+            }
+
+
+        }
+        if (arrVagons.size()==0) {System.out.println("Find 0 vagons ");};
+        return arrVagons;
+    }
+
+}
